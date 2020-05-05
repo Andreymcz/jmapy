526a58a691bcb28666e89b26c8f5c540dac4c5b9
unknown <andreymcz@gmail.com>
2020-05-05 00:16:10 -0300

    Getting chemspider info from raw http requests

--- chemspi_local_db.py
+++ chemspi_local_db.py
@@ -7,9 +7,25 @@ class ChemspiLocalDB:
         self.database = pd.read_excel(filename)
         self.db_names = self.database.loc[:,self.compound_name_column_label].copy().str.lower().sort_values()
     
-    def find_compound_by_name(self, compound_name):
+    def find_compounds_by_name_mass(self, compound_name, compound_mass, equal_mass_tolerance_percent = 0.001):
+        TOLERATED_ERROR = compound_mass * equal_mass_tolerance_percent
+        
         compound_name = compound_name.lower()
-        search_name_idx = self.db_names.searchsorted(compound_name)    
+        search_name_idx = self.db_names.searchsorted(compound_name)
+        
+        while search_name_idx < self.db_names.shape[0]:
+            db_row = self.db_names.index[search_name_idx]
+            db_entry = self.database.iloc[db_row]
+            #print("Candidate: ",db_entry.Name.lower(), " ", db_entry['Molecular Weight'])
+            if db_entry.Name.lower() != compound_name:
+                return None
+            else:
+                # check mass                
+                if compound_mass > 0 and abs(db_entry['Molecular Weight'] - compound_mass) < TOLERATED_ERROR:
+                    return db_entry
+            
+            search_name_idx += 1
+        
     
         if (search_name_idx < self.db_names.shape[0]):
             db_row = self.db_names.index[search_name_idx]
--- chemspi_web_db.py
+++ chemspi_web_db.py
@@ -1,4 +1,7 @@
 from chemspipy import ChemSpider
+from bs4 import BeautifulSoup
+import requests
+
 # Chemspi web api utilities
 class ChemspiWebDB:
     def __init__(self, api_key):
@@ -8,13 +11,14 @@ class ChemspiWebDB:
     # search compounds that matches name, mass criteria. Return list of matching csid's and a list of warnings
     def find_compounds_by_name_mass(self, target_compound_name, target_compound_mass = 0, equal_mass_tolerance_percent = 0.001):
         TOLERATED_ERROR = target_compound_mass * equal_mass_tolerance_percent
-
+        print("find_compounds_by_name_mass")
         warnings = []
         compound_csids = []
         
         # search compound by name
         found_compounds = self.chemspider_web_api.search(target_compound_name)
         found_compounds.wait()
+        print(found_compounds)
 
         if not found_compounds.success():
             warnings.append("Compound search failed")
@@ -25,12 +29,13 @@ class ChemspiWebDB:
         if found_compounds.count == 0:
             warnings.append("Compound Not found in chemspider web search")
 
+        print("sucess")
         for result in found_compounds :
             try:
-                #print("Result: ")
-                #print("ID: ", result.record_id)
-                ##print("Molecular Formula: ", result.molecular_formula)
-                #print("Monoisotopic Mass: ", result.monoisotopic_mass)
+                print("Result: ")
+                print("ID: ", result.record_id)
+                print("Molecular Formula: ", result.molecular_formula)
+                print("Monoisotopic Mass: ", result.monoisotopic_mass)
                 if target_compound_mass > 0 and abs(result.monoisotopic_mass - target_compound_mass) > TOLERATED_ERROR :
                     #print("Mass too diferent, skipping")                    
                     continue # mass too diferent from target, continue search
@@ -62,4 +67,124 @@ class ChemspiWebDB:
             results[db_name].append(db_comp_id)
         
         return results, warnings
+
+from dataclasses import dataclass    
+@dataclass
+class ChemspiCompoundInfo:
+        csid: int = -1
+        name: str = ""
+        molecular_formula: str = ""
+        monoisotopic_mass: float = 0.0
+        iupac_name: str = "NOT FOUND"
+            
+
+def __http_req(address):
+    print("Waiting for http request: ", address, "...", end='')     
+    r = requests.get(address)
+    print("Done");
+    return BeautifulSoup(r.text, 'html.parser')
+            
+def __search_compound_by_name(name):
+    addr = 'http://www.chemspider.com/Search.aspx?q=' + name
+    return __http_req(addr)
+    
+def __search_compound_by_id(id):
+    addr = 'http://www.chemspider.com/Chemical-Structure.' + str(id) + ".html";
+    return __http_req(addr)
+            
+def __extract_single_compound(single_request_result):
+    compound_info = ChemspiCompoundInfo()
+    
+    structure_header = single_request_result.find(id = "ctl00_ctl00_ContentSection_ContentPlaceHolder1_RecordViewDetails_rptDetailsView_ctl00_structureHead")    
+    details = single_request_result.find(id = "ctl00_ctl00_ContentSection_ContentPlaceHolder1_RecordViewTabDetailsControl_identifiers_ctl_synonymsControl_SynonymsPanel")
+    
+    #print(details)
+    #print(structure_header.prettify())
+    # db name
+    db_name = structure_header.find(id = "ctl00_ctl00_ContentSection_ContentPlaceHolder1_RecordViewDetails_rptDetailsView_ctl00_WrapTitle")
+    compound_info.name = db_name.get_text()
+    
+    for li in structure_header.find_all('li'):
+        if li.span and li.span.has_attr('class') and li.span['class'][0] == "prop_title":
+            ptitle = li.span.string
+            if ptitle == "Molecular Formula":
+                compound_info.molecular_formula = li.contents[1].get_text()
+            elif ptitle == "Monoisotopic mass":
+                compound_info.monoisotopic_mass = float(str(li.contents[1]).split(" ")[0])
+            elif ptitle == "ChemSpider ID":
+                compound_info.csid = int(li.contents[1])
+
+    for syn in details.find_all('div', class_="syn", recursive=False):        
+        SYN_REF_TARGET = "[ACD/IUPAC Name]"
+
+        language = syn.find('span', class_='synonym_language')        
+        syn_ref = syn.find('span', class_='synonym_ref')
+        if syn_ref != None and syn_ref.string.strip() == SYN_REF_TARGET and language == None:
+            if syn.strong:
+                compound_info.iupac_name = syn.strong.get_text()
+            elif syn.find('span', class_='synonym_cn'):
+                compound_info.iupac_name = syn.find('span', class_='synonym_cn').get_text()
+            else:
+                compound_info.iupac_name = "NOT FOUND"
+            
+    
+    return compound_info
+
+def http_find_compounds_by_name_mass(compound_name, target_compound_mass, equal_mass_tolerance_percent = 0.001):
+    TOLERATED_ERROR = target_compound_mass * equal_mass_tolerance_percent
+    #print(soup.prettify())
+    search_result = __search_compound_by_name(compound_name)
+
+    single_result = search_result.find(id = "ctl00_ctl00_ContentSection_ContentPlaceHolder1_RecordViewDetails_rptDetailsView_ctl00_pnlDetailsView")
+    table_results = search_result.find(id="ctl00_ctl00_ContentSection_ContentPlaceHolder1_ResultViewControl1_grid_GridView1")
+
+    if single_result :
+        c = __extract_single_compound(search_result)
+        if target_compound_mass > 0 and abs(c.monoisotopic_mass - target_compound_mass) > TOLERATED_ERROR :
+            return []
+        return [c]
+    elif table_results:
+        # filter results by mass, then search from result_id page
+        compounds = []
+        for td in table_results.find_all('td', class_='search-id-column'):
+            id = td.a.string.strip()
+            c = __extract_single_compound(__search_compound_by_id(id))
+            if target_compound_mass > 0 and abs(c.monoisotopic_mass - target_compound_mass) > TOLERATED_ERROR :
+                continue
+            compounds.append(c)
+        
+        return compounds
+    else:
+        print("no results")
+        return []
+
+def http_find_compound_by_id(compound_id):
+    return __extract_single_compound(__search_compound_by_id(id), compound_mass)
+
+
+def http_find_compound_external_sources_ids(compound_id, external_sources_list):
+    
+    external_sources_ids = dict()
+    for es in external_sources_list:
+        external_sources_ids[es] = []
+        
+    request_response = __http_req('http://www.chemspider.com/ibcontent.ashx?csid=' + str(compound_id) + '&type=ds')
+    if not request_response.find('table'):
+        return external_sources_ids
+    
+    
+    for tr in request_response.table.tbody.find_all('tr'):
+        entries = tr.find_all('td')
+        es_name = entries[0].a.get_text()
+        if es_name in external_sources_ids:
+            es_ids =  entries[1]
+            for esid in es_ids.find_all('a'):
+                external_sources_ids[es_name].append(esid.get_text())
+                
+    return external_sources_ids
+            
+        
+        
+        
+    
         
